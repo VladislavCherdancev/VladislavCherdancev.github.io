cbc1a1a6df99b07688dd6cf4e7df80abb238b63c
VladislavCherdancev <105554420+VladislavCherdancev@users.noreply.github.com>
2022-05-31 18:53:00 +0500

    Add files via upload

--- src/js/myScript.js
+++ src/js/myScript.js
@@ -1,33 +1,38 @@
 "use strict"
 
- function myFunction() {
-  var x,y,z,oper;
-  x=prompt("Какой сайт Вас интересует");
-  y=prompt("В какие сроки нужно уложиться");
-  oper=prompt("Адаптивность для ПК или еще для мобильных устройств");
-  
-  if(oper=="+"){
-    z=Number(x)+Number(y);
+  function calc(){
+
+  var  siteType , design , adaptability, sum = 0;
+   
+   let sum = 0;
+      
+  let siteType = prompt( "Тип сайта.  1.коммерческий, 2.информационный, 3.социальный");
+                       
+  let design = prompt( "Дизай. 1.классический, 2.минимализм, 3.рисованный");
+
+  let adaptability = prompt( "Адаптивность. 1.только ПК, 2.только мобильные телефоны, 3.ПК и мбольные телефоны");
+   
+   if (siteType == "1") {sum = sum + 1000};
+      
+     if (siteType == "2") {sum = sum + 1500};
+      
+     if (siteType == "3") {sum = sum + 2000};
+     
+      
+       if (design == "1") {sum = sum + 500};
+      
+     if (design == "2") {sum = sum + 700};
+      
+     if (design == "3") {sum = sum + 900};
+      
+      
+       if (adaptability == "1") {sum = sum + 100};
+      
+     if (adaptability == "2") {sum = sum + 150};
+      
+     if (adaptability == "3") {sum = sum + 200};
+      
+      alert(sum);
   }
-  else if(oper=!null){
-    z="Error"
-  }
-  else if(x=!null){
-    z="Error"
-  }
-  else if(y=!null){
-    z="Error"
-  }
-  else if(oper=="/"){
-    z=Number(x)/Number(y);
-  }
-  else if(oper=="*"){
-    z=Number(x)*Number(y);
-  }
-  else if(oper=="-"){
-    z=Number(x)-Number(y);
-  }
-  
-  return z;
-    }
-document.getElementById("myScript").innerHTML =myFunction();
+     
+  calc();
