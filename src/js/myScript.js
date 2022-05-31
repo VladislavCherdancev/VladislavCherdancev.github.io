93d1e57010c79e4bea22220d5ba7816c417add94
VladislavCherdancev <105554420+VladislavCherdancev@users.noreply.github.com>
2022-05-31 19:31:55 +0500

    Add files via upload

--- src/js/myScript.js
+++ src/js/myScript.js
@@ -3,8 +3,6 @@
   function calc(){
 
   var  siteType , design , adaptability, sum = 0;
-   
-  let sum = 0;
       
   let siteType = prompt( "Тип сайта.  1.коммерческий, 2.информационный, 3.социальный");
                        
@@ -12,27 +10,28 @@
 
   let adaptability = prompt( "Адаптивность. 1.только ПК, 2.только мобильные телефоны, 3.ПК и мбольные телефоны");
    
-   if (siteType == "1") {sum = sum + 1000};
+     if (siteType == "1") {sum = sum + 1000};
       
      if (siteType == "2") {sum = sum + 1500};
       
      if (siteType == "3") {sum = sum + 2000};
      
       
-       if (design == "1") {sum = sum + 500};
+     if (design == "1") {sum = sum + 500};
       
      if (design == "2") {sum = sum + 700};
       
      if (design == "3") {sum = sum + 900};
       
       
-       if (adaptability == "1") {sum = sum + 100};
+     if (adaptability == "1") {sum = sum + 100};
       
      if (adaptability == "2") {sum = sum + 150};
       
      if (adaptability == "3") {sum = sum + 200};
       
       alert(sum);
+      
   }
      
   calc();
