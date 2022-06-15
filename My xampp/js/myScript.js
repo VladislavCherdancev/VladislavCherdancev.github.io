669bd7de910a9a0cdf28a1f43f0c591690c09dd7
VladislavCherdancev <105554420+VladislavCherdancev@users.noreply.github.com>
2022-06-15 19:28:21 +0500

    Add files via upload

--- My xampp/js/myScript.js
+++ My xampp/js/myScript.js
@@ -3,8 +3,8 @@
 /*таймер на модальное окно
 setTimeout(function(){ 
     $("#modalopen").click();
-}, 10000); */
-
+}, 10000); 
+*/
  /*Маска на мои контакты*/ 
 
 $(document).ready(function($){
@@ -137,8 +137,6 @@ $("select").on("change", function(){
 
 /*скрипт на отправку формы*/
  $(document).ready(function(){
-     
- 
   $('form').submit(function(event){
       event.preventDefault();
       $.ajax({
@@ -157,38 +155,18 @@ $("select").on("change", function(){
   
 
 /*Расчет стоимости*/
-// Created for an Articles on:
-// https://www.html5andbeyond.com/bubbling-text-effect-no-canvas-required/
-
 jQuery(document).ready(function($){
- 
-    // Define a blank array for the effect positions. This will be populated based on width of the title.
     var bArray = [];
-    // Define a size array, this will be used to vary bubble sizes
     var sArray = [4,6,8,10];
- 
-    // Push the header width values to bArray
     for (var i = 0; i < $('.bubbles').width(); i++) {
         bArray.push(i);
     }
-     
-    // Function to select random array element
-    // Used within the setInterval a few times
     function randomValue(arr) {
         return arr[Math.floor(Math.random() * arr.length)];
     }
- 
-    // setInterval function used to create new bubble every 350 milliseconds
-    setInterval(function(){
-         
-        // Get a random size, defined as variable so it can be used for both width and height
+    setInterval(function(){   
         var size = randomValue(sArray);
-        // New bubble appeneded to div with it's size and left position being set inline
-        // Left value is set through getting a random value from bArray
         $('.bubbles').append('<div class="individual-bubble" style="left: ' + randomValue(bArray) + 'px; width: ' + size + 'px; height:' + size + 'px;"></div>');
-         
-        // Animate each bubble to the top (bottom 100%) and reduce opacity as it moves
-        // Callback function used to remove finsihed animations from the page
         $('.individual-bubble').animate({
             'bottom': '100%',
             'opacity' : '-=0.7'
@@ -196,14 +174,8 @@ jQuery(document).ready(function($){
             $(this).remove()
         }
         );
- 
- 
     }, 350);
- 
 });
-/*Расчет стоимости*/
-
-
 
 
 
